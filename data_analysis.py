ce25f3562895363dda4556faa655d18bcc9a1647
ThomasAIST <lokhin.y@gmail.com>
2025-03-30 17:02:33 +0800

    created draw_pie_chart function and filter_dataset function

--- data_analysis.py
+++ data_analysis.py
@@ -2,6 +2,7 @@ from train import load_and_tokenize_data
 from imblearn.over_sampling import SMOTE
 import numpy as np
 from collections import defaultdict
+from datasets import Dataset
 
 dataset, label_names = load_and_tokenize_data()
 print(label_names)
@@ -62,3 +63,30 @@ plt.title("Frequency of Number of Non-Entities per Sentence")
 plt.xlabel("Number of Non-Entities")
 plt.ylabel("Frequency")
 plt.show()
+
+
+
+def draw_pie_chart():
+    # label, size = [], []
+    # total = sum(y for x, y in counting.items())
+    total = total_entities+counting[label_names[ner_tag]]
+    # for x, y in counting.items():
+    #     label.append(x)
+    #     size.append(y/total*100)
+    size = [total_entities/total, counting[label_names[ner_tag]]/total]
+    label = ['Entity', 'Non-entity']
+    color = ['lightblue', 'red']
+    plt.pie(size, explode=(0.1, 0), labels=label, colors=color, autopct='%1.1f%%')
+    plt.show()
+
+# Test
+# draw_pie_chart()
+
+def filter_dataset(k, dataset: Dataset):
+    filtered = dataset.filter(lambda data: len(data["ner_tags"])-data["ner_tags"].count(0) >= k)
+    return filtered
+
+# Test 
+# filtered_dataset = filter_dataset(5, training_dataset)
+# print(filtered_dataset["ner_tags"])
+# print(training_dataset["ner_tags"])
